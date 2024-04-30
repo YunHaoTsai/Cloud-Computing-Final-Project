518eb06d6438dfd33649d408aec750f02a18a7c4
Huang Che Yu <jerry378jerry@gmail.com>
2024-04-30 11:06:10 +0800

    test

--- Crawling.py
+++ Crawling.py
@@ -13,6 +13,7 @@ def show_probable():
     games = soup.find_all('div',class_=lambda x: x and 'probable-pitchers__matchup probable-pitchers__matchup' in x)
 
     probable.append("# "+today+" Game Schedule")
+    probable.append("## "+str(len(games))+" Games")
     for game in games:
         match_information = "```"
         away_team = game.find('span',class_="probable-pitchers__team-name--away").text.strip()
@@ -29,7 +30,7 @@ def show_probable():
             i += 1
         match_information += "-"*70+"\n"
         if pitchers[1] == "TBD":
-            match_information += "{:10}{:9}{:20}\n".format(away_team,team_record[1],pitchers[1])
+            match_information += "{:10}{:9}{:20}\n".format(home_team,team_record[1],pitchers[1])
         else:
             match_information += "{:10}{:9}{:20}{} {}\n".format(home_team,team_record[1],pitchers[1],pitch_hands[i],pitch_summary[i])
         match_information += '```'
